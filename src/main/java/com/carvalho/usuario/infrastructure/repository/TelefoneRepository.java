41fc8f943b21be742b66427ee71aa7d3ee8d725a
Will Rodrigues <wilsonrodriguessantos2@gmail.com>
2025-07-22 12:15:44 -0300

    adicionado método que salva dados usuário

--- src/main/java/com/carvalho/usuario/infrastructure/repository/TelefoneRepository.java
+++ src/main/java/com/carvalho/usuario/infrastructure/repository/TelefoneRepository.java
@@ -1,6 +1,6 @@
 package com.carvalho.usuario.infrastructure.repository;
 
-import com.wilson.aprendendo_spring.infrastructore.entity.Telefone;
+import com.carvalho.usuario.infrastructure.entity.Telefone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
